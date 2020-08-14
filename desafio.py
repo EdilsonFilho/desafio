cd50f93c5f2d12b8c7752e0ab72a90300fecc6dc
EdilsonFilho <edilsonfilhosh@gmail.com>
2020-08-13 23:01:16 -0300

    percorrendo o json em camadas

--- desafio.py
+++ desafio.py
@@ -15,18 +15,6 @@ from pprint import pprint
 def main():
 
     print('###########Consulta na API#############')
-
-    requ = requests.get('http://compras.dados.gov.br/compraSemLicitacao/doc/item_slicitacao/15871907000012019/itens.json')
-    tit = json.loads(requ.text)
-    tot = tit['_embedded']
-    tat = tot['compras']
-
-    print(tit['_embedded']['compras'][0]['ds_detalhada'])
-    
-   
-
-       
-       
     con = psycopg2.connect(
         user = "postgres",
         password = "aryanalinda",
@@ -41,9 +29,22 @@ def main():
     rows = cursor.fetchall()
     
     for r in rows:
-        request = requests.get('http://compras.dados.gov.br{}'.format(r[1]))
-        print(request)
         print(f"valor R$ {r[0]} e o descritivo do iten está em: {r[1]}")
+        request = requests.get('http://compras.dados.gov.br{}.json'.format(r[1]))
+        #print(request)
+        # requ = requests.get('http://compras.dados.gov.br/compraSemLicitacao/doc/item_slicitacao/15871907000012019/itens.json')
+        tit = json.loads(request.text)
+        print('---Mostrando ITENS-----')
+        print(tit['_embedded']['compras'][0]['ds_detalhada'])
+        print('--- FIM DE Mostrando ITENS-----')
+        print('#*#*#*#*#*#*#*#')
+        print('---Mostrando VALOR-----')
+        print('o valor do contrato foi  de R$ {}'.format(tit['_embedded']['compras'][0]['vr_estimado']))
+        print('--- FIM DE Mostrando VALOR-----')
+        print('#*#*#*#*#*#*#*#')
+        print('---Mostrando FORNECEDOR-----')
+        print(tit['_embedded']['compras'][0]['_links']['self'])
+        print('--- FIM DE Mostrando FORNECEDOR-----')
 
     
     con.close()

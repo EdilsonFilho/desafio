fec68b7689371602e773d6df9051c8307950df39
EdilsonFilho <edilsonfilhosh@gmail.com>
2020-08-14 17:06:55 -0300

    criacao de menu e resolucoes

--- desafio.py
+++ desafio.py
@@ -12,46 +12,72 @@ from sqlalchemy.sql import func
 #app =Flask(__name__)
 #app.config['DEBUG']= True
 #db = SQLAlchemy()
-
-def main():
-
-    print('###########Consulta na API#############')
-    con = psycopg2.connect(
+print('########################################')
+print('#   CONSULTA USANDO API E POSTGRES     #')
+print('#    VISAO GERAL DA PESQUISA           #')
+print('# 10 COMPRAS REALIZADAS SEM LICITACAO  #')
+print('#          ANO DE 2019                 #')
+print('########################################')
+con = psycopg2.connect(
         user = "postgres",
         password = "aryanalinda",
         host = "localhost",
         port = "5432",
         dbname = "postgres")
-    print("CONEXÃO COM BD DO POSTGRESQL REALIZADA COM SUCESSO!")
+print("!!SUCESSO NA CONEXÃO COM BD!!")
+print("\n\n")
+
+def main():
+    
+    option = int(input('Qual consulta desejas realizar?\n 1.Maior valor de licitacao\n 2.Menor valor de licitacao\n 3.Soma total do valor usado sem licitacao em 2019\n 4.Média do valor usado sem licitacao em 2019\n 5. Informações gerais\n 6.Sair\n'))
 
     cursor = con.cursor()
     cursor.execute("select valor,itens,fornecedor,quantidade, n_servico,materiais_contratados from compras_slicitacao;")
     rows = cursor.fetchall()
-    
-
-    print(' VISAO GERAL DA PESQUISA')
-    print(' COM 10 COMPRAS REALIZADAS SEM LICITACAO PERIODO DE 2019')
-    for r in rows:
-       
-        print(f"valor R$ {r[0]} e o link do iten está em: {r[1]}")
-        print(f"O fornecedor é: {r[2]}, a quantidade é : {r[3]}")
-        print(f"O número de serviço é : {r[4]}, e o material contratado é : {r[5]}")
-        print('--------------------------------------------------------------------')
-    
     s = 0
+    6
+    maior=0
+    menor=0
+    #optcao de consulta para maior e menor
+    for c in rows:
+        aux =  c[0]
+        if maior < aux:
+            maior = aux
+            aux=0
+        elif maior > aux:
+            menor = aux
+    #optcao de consulta para soma e media
     for c in rows:
         n = c[0]
         s += n
-    print('Somatório dos valores: R$ {}'.format(s))
-    print('Valro médio das compras: R$ {}'.format(s/10))
 
-    for c in rows:
-        n = c[0]
+    if option == 1:
+      print('Valor maior é R$ {:.2f}\n'.format(maior))
+      option = 7
+    if option == 2:
+      print('Valor menor é R$ {:.2f}\n'.format(menor))
+      option = 7
+    if option == 3:
+     print('Somatório dos valores: R$ {}\n'.format(s))
+     option = 7
+    if option == 4:
+        print('Valor médio das compras: R$ {:.2f}\n'.format(s/10))
+        option = 7
+
+    if option == 5:
+        for r in rows:
         
-    print('Somatório dos valores: R$ {}'.format(s))
-    print('Valro médio das compras: R$ {}'.format(s/10))
-
-
+            print(f"valor R$ {r[0]} e o link do iten está em: {r[1]}")
+            print(f"O fornecedor é: {r[2]}, a quantidade é : {r[3]}")
+            print(f"O número de serviço é : {r[4]}, e o material contratado é : {r[5]}")
+            print('--------------------------------------------------------------------\n')
+            option = 7
+    if option == 6:
+        print('SAINDO DA APLICAÇÃO')
+        print('-------------------')
+    if option == 7:
+        main()
+   
 
     '''
     for r in rows:

5ab9687a1118ccdfc3e7d63ecba6dc9e5b96560c
EdilsonFilho <edilsonfilhosh@gmail.com>
2020-08-13 21:28:04 -0300

    conexao python com postgres

--- desafio.py
+++ desafio.py
@@ -1,5 +1,9 @@
 import csv
 import requests
+import psycopg2
+import json
+from psycopg2 import Error
+from pprint import pprint
 
 
 
@@ -10,11 +14,41 @@ import requests
 
 def main():
 
-    print('##Consulta na API###')
+    print('###########Consulta na API#############')
 
-    request = requests.get('http://compras.dados.gov.br/compraSemLicitacao/v1/compras_slicitacao.json?co_orgao=26233&dt_ano_aviso_licitacao=2019')
+    requ = requests.get('http://compras.dados.gov.br/compraSemLicitacao/doc/item_slicitacao/15871907000012019/itens.json')
+    tit = json.loads(requ.text)
+    tot = tit['_embedded']
+    #tat = tot['compras']
 
-    print(request.json())
+    print(tot['compras'][1]['co_servico'][2])
+    
+   
+
+       
+       
+    con = psycopg2.connect(
+        user = "postgres",
+        password = "aryanalinda",
+        host = "localhost",
+        port = "5432",
+        dbname = "postgres")
+    print("Successfully connected!")
+
+    cursor = con.cursor()
+    cursor.execute("select valor,itens from compras_slicitacao;")
+   
+    rows = cursor.fetchall()
+    
+    for r in rows:
+        request = requests.get('http://compras.dados.gov.br{}'.format(r[1]))
+        print(request)
+        print(f"valor R$ {r[0]} e o descritivo do iten está em: {r[1]}")
+
+    
+    con.close()
+    
+    
 
 if __name__ == '__main__':
     main()
